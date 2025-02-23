4a42c7876aeec8f997bee205dd41ba7e59ff97df
kristinadd <kristina@productboard.com>
2025-02-23 16:56:33 +0100

    DAO Factory

--- src/main/java/com/kristina/ecom/dao/DAOFactory.java
+++ src/main/java/com/kristina/ecom/dao/DAOFactory.java
@@ -1,5 +1,24 @@
 package com.kristina.ecom.dao;
 
 public class DAOFactory {
-  
+  public static DAO getDAO(DAOType daoType) {
+    if (daoType == null) {
+      return null;
+    }
+
+    switch (daoType) {
+      case DAOType.ORDER_MONGO:
+        return new OrderDAOMongo();
+      case DAOType.ORDER_SQL:
+        return new OrderDAOMySql();
+      case DAOType.PRODUCT_MONGO:
+        return new ProductDAOMongo();
+      case DAOType.PRODUCT_SQL:
+        return new ProductDAOMySql();
+      case DAOType.SHOPPING_CART_MONGO:
+        return new ShoppingCartDAOMongo();
+      default:
+        throw new IllegalArgumentException("Unknown dao type: " + daoType);
+    }
+  }
 }
---
+++ src/main/java/com/kristina/ecom/dao/DAOType.java
@@ -0,0 +1,9 @@
+package com.kristina.ecom.dao;
+
+public enum DAOType {
+  ORDER_MONGO,
+  ORDER_SQL,
+  PRODUCT_MONGO,
+  PRODUCT_SQL,
+  SHOPPING_CART_MONGO
+}
